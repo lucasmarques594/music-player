fa6530afd43e6ac775951870ca68bb465eb0884c
Shine <63941290+lucasmarques594@users.noreply.github.com>
2023-04-19 14:56:08 -0300

    Add files via upload

---
+++ assets/scripts/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+    {
+        name: "28",
+        artist: "Bulma",
+        img: "music-1",
+        src:"music-1"
+    },
+    {
+        name: "hmpf",
+        artist: "Zuri",
+        img: "music-2",
+        src:"music-2"
+    },
+    {
+        name: "CONTROL",
+        artist: "envy",
+        img: "music-3",
+        src:"music-3"
+    },
+    {
+        name: "P.S fck you cunt",
+        artist: "Lil Peep",
+        img: "music-4",
+        src:"music-4"
+    },
+    {
+        name: "mortal",
+        artist: "ABSEBT191",
+        img: "music-5",
+        src:"music-5"
+    },
+    {
+        name: "trance",
+        artist: "sp3ctrl",
+        img: "music-6",
+        src:"music-6"
+    },
+];
